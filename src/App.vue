1eda5556a5602065db43d173fdf7ed5ad20e5806
Raul Garcia Ameyugo <rgarcia@gemweb.es>
2023-10-03 20:05:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Basic template</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import bootstrap from 'bootstrap'
+  export default {
+    name: 'App',
+    components: {bootstrap}
+  }
+</script>
+<style lang="scss">
+  //  @import "/node_modules/bootstrap/scss/bootstrap.scss";
+  @import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+</style>
